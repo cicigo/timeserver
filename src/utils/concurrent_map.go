334d025134a954d1040b718d9ea4a409e91c55e7
cicigo <christinazhang0730@gmail.com>
2015-02-16 00:15:23 -0800

    Concurrent map module

---
+++ src/utils/concurrent_map.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"sync"
+)
+
+type ConcurrentMap struct {
+	data  map[string]string
+	mutex *sync.Mutex
+}
+
+func (m *ConcurrentMap) Get(key string) string {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	return m.data[key]
+}
+
+func (m *ConcurrentMap) Put(key string, value string) {
+	m.mutex.Lock()
+	m.data[key] = value
+	m.mutex.Unlock()
+}
+
+func NewConcurrentMap() *ConcurrentMap {
+	return &ConcurrentMap{
+		data:  make(map[string]string),
+		mutex: new(sync.Mutex),
+	}
+}
